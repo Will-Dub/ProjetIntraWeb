e0b6095e7418799e02710fe19af245c674b97548
William Dubuc <william.dubuc01@gmail.com>
2023-11-04 13:21:54 -0400

    Added cards images and background

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
     if($.cookie('solde') === undefined || $.cookie('email') === undefined){
         window.location.replace("index.html");
     }
+
+    refreshSolde();
 })
